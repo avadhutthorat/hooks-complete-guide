f46401c3859db48577ff5a827c51551f13b8c75e
avadhutthorat <avadhutthorat40@gmail.com>
2019-10-26 20:56:02 +0530

    added useState with previous state feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import ClassCounter from "./components/class-counter";
 import HookCounter from "./components/hook-counter";
+import HookCounter1 from "./components/hook-counter1";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <ClassCounter />
       <hr />
       <HookCounter />
+      <hr />
+      <HookCounter1 />
     </div>
   );
 }
---
+++ src/components/hook-counter1.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const HookCounter1 = () => {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+  return (
+    <div>
+      <label htmlFor="count">Count {count}</label> <br />
+      <button onClick={() => setCount(initialCount)}>Reset</button> <br />
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Increment by 1{" "}
+      </button>{" "}
+      <br />
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>
+        Decrement by 1{" "}
+      </button>{" "}
+      <br />
+      <button onClick={() => setCount(prevCount => prevCount + 5)}>
+        Increment by 5{" "}
+      </button>{" "}
+      <br />
+    </div>
+  );
+};
+
+export default HookCounter1;
